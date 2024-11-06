a826c2f98243efdd19bce43e02c2bd4a55062aaf
Evando Tomas <evandrotomas@hotmail.com>
2024-11-05 22:19:03 -0300

    chore: add prisma and database setup

---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+const None = () => {
+  return <h1>hello word</h1>
+}
+
+export default None
---
+++ app/subscriptions/page.tsx
@@ -0,0 +1,5 @@
+const Subscriptions = () => {
+    return <h1>subscriptions page</h1>
+}
+
+export default Subscriptions
