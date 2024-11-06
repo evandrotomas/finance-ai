d8c90ba3c1a9b376f4f76a39d96258cd5252da78
Evando Tomas <evandrotomas@hotmail.com>
2024-11-06 20:47:25 -0300

    chore: add prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-const None = () => {
-  return <h1>hello word</h1>
-}
+const Home = () => {
+  return <h1 className="p-5 text-red-500">hello word</h1>;
+};
 
-export default None
+export default Home;
