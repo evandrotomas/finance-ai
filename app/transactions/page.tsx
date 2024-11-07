409aa05a02e0f10858185d302b2d52207e277f18
Evando Tomas <evandrotomas@hotmail.com>
2024-11-07 20:54:33 -0300

    feat: add transaction type dabge

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,25 @@
-const TransactionsPage = () => {
-  return <h1>transactions page</h1>;
+import { ArrowDownUp } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { TransactionColumns } from "./_columns/indes";
+
+const TransactionsPage = async () => {
+  // acessar as transações do meu banco de dados
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      {/* {TITULO E BOTAO} */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xls font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adcionar transações
+          <ArrowDownUp />
+        </Button>
+      </div>
+      <DataTable columns={TransactionColumns} data={transactions} />
+    </div>
+  );
 };
 
 export default TransactionsPage;
